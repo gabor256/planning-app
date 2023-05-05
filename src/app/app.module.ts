6df20d75008fa5d490f003ebb5305a90a67c3558
Roszjár Gábor <groszjar@porschehungaria.hu>
2023-05-05 15:45:03 +0200

    Modulok létrehozása - Auth Feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule } from "@angular/common/http";
-import { AuthComponent } from "./auth/auth.component";
 import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
+import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule, //Ez kell ha nem Template driven Formot használunk
     HttpClientModule,
     AppRoutingModule,
     CoreModule,
     SharedModule,
+    AuthModule,
     RecipesModule,
     ShoppingListModule,
   ],
