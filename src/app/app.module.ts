8d7a1fcc443611b6211b6ed31ebd9a4e15f4e54d
Roszjár Gábor <groszjar@porschehungaria.hu>
2023-05-05 16:05:09 +0200

    Lazy loading használata #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,8 @@ import { HeaderComponent } from './header/header.component';
 import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule } from "@angular/common/http";
-import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
-import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
   declarations: [
@@ -23,8 +21,6 @@ import { AuthModule } from "./auth/auth.module";
     AppRoutingModule,
     CoreModule,
     SharedModule,
-    AuthModule,
-    ShoppingListModule,
   ],
   bootstrap: [AppComponent]
 })
