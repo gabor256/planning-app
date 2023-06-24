d8cf7001e22f3c3a37d1e5de115914423b3e5e38
Roszjar Gabor <roszjar.gabor@gmail.com>
2023-06-24 15:09:13 +0200

    ngrx: update and delete ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,6 +3,8 @@ import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "./shopping-list.service";
 import {Observable, Subscription} from "rxjs";
 import {Store} from "@ngrx/store";
+import * as ShoppingListActions from "./store/shopping-list.actions";
+import * as fromShoppingList from "./store/shopping-list.reducer";
 
 @Component({
   selector: 'app-shopping-list',
@@ -15,7 +17,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   constructor(
     private shoppingListService: ShoppingListService,
-    private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) {
+    private store: Store<fromShoppingList.AppState>) {
   }
 
   ngOnInit() {
@@ -25,7 +27,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   onEditItem(index: number) {
-    this.shoppingListService.startedEditing.next(index);
+    // this.shoppingListService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
 
   }
 
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,21 +1,55 @@
 import {Ingredient} from "../../shared/ingredient.model";
 import * as ShoppingListActions from "./shopping-list.actions";
 
-const initialState = {
+export interface AppState {
+  shoppingList: State;
+}
+
+export interface State {
+  ingredients: Ingredient[];
+  editedIngredient: Ingredient;
+  editedIngredientIndex: number;
+}
+
+const initialState: State = {
   ingredients: [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
-  ]
+  ],
+  editedIngredient: null,
+  editedIngredientIndex: -1,
 };
 
-export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+export function shoppingListReducer(state: State = initialState, action: ShoppingListActions.ShoppingListActions) {
   switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
-      return {...state /* Ez a státusz másolása */, ingredients: [...state.ingredients, action.payload] };
+      return {...state /* Ez a státusz másolása */, ingredients: [...state.ingredients, action.payload]};
     case ShoppingListActions.ADD_INGREDIENTS:
       // Ez itt egy érdekes sztori: hozzáadja az eredeti ingredients listát majd a másik listát is
       // az a trükk, hogy mindent kibontva darabonként az hozzá, arra való a 3 db pont előtte
       return {...state, ingredients: [...state.ingredients, ...action.payload]};
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[state.editedIngredientIndex];
+      // Itt a update-ben átmásoljuk az eredetit és felülírjuk az újjal
+      const updatedIngredient = {...ingredient, ...action.payload};
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
+      return {...state, ingredients: updatedIngredients, editedIngredient: null, editedIngredientIndex: -1};
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients
+          .filter((ingredient, ingredientIndex) => ingredientIndex !== state.editedIngredientIndex),
+        editedIngredient: null,
+        editedIngredientIndex: -1
+      };
+    case ShoppingListActions.START_EDIT:
+      return {
+        ...state,
+        editedIngredientIndex: action.payload,
+        editedIngredient: {...state.ingredients[action.payload]}};
+    case ShoppingListActions.STOP_EDIT:
+      return {...state, editedIngredient: null, editedIngredientIndex: -1};
     default:
       return state;
   }
