d7e9ccab2dd823e890d0232e7612412c48b01bc9
Roszjar Gabor <roszjar.gabor@gmail.com>
2023-06-30 14:00:54 +0200

    ngrx: auth bugfix

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -11,14 +11,14 @@ const initialState: State = {
 
 export function authReducer(state = initialState, action: AuthActions.AuthActions) {
   switch (action.type) {
-    case "LOGIN":
+    case AuthActions.LOGIN:
       const user = new User(
         action.payload.email,
         action.payload.userId,
         action.payload.token,
         action.payload.expirationDate);
       return {...state, user: user};
-    case "LOGOUT":
+    case AuthActions.LOGOUT:
       return {...state, user: null};
     default:
       return state;
