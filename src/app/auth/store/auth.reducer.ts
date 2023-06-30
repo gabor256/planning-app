3727ade9d6f83e38f5b6626900fca32c8ec9773a
Roszjar Gabor <roszjar.gabor@gmail.com>
2023-06-30 14:20:06 +0200

    ngrx: ngrx effects, first effect - ez az ngrx-ben az observable kezeléshez használatos, azaz pl. adatbázis kezeléshez

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -11,7 +11,7 @@ const initialState: State = {
 
 export function authReducer(state = initialState, action: AuthActions.AuthActions) {
   switch (action.type) {
-    case AuthActions.LOGIN:
+    case AuthActions.LOGIN_SUCCESS:
       const user = new User(
         action.payload.email,
         action.payload.userId,
